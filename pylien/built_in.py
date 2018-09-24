c9282a580e002aa3cab43d7c31c2a87ccde6a387
m0cchi <boom.boom.planet@gmail.com>
2018-09-24 20:48:15 +0900

    impl defvar

--- pylien/built_in.py
+++ pylien/built_in.py
@@ -1,4 +1,4 @@
-from pylien.model import Function, Unit, AtomicType, env
+from pylien.model import Function, Macro, Unit, AtomicType, env
 
 class PrintFn(Function):
     def __init__(self):
@@ -13,5 +13,22 @@ class PrintFn(Function):
         sys.stdout.flush()
         print()
         return Unit(AtomicType.NIL)
-        
+
+class DefvarMc(Macro):
+    def __init__(self):
+        super(DefvarMc, self).set_args(['defvar name', 'defvar value'])
+
+    def call(self, env, body):
+        env = super(DefvarMc, self).parse_args(env, body)
+        name = env.get_variable(self.args[0])
+        value = env.get_variable(self.args[1])
+
+        from pylien.parser.semantic import SemanticAnalyzer
+        s = SemanticAnalyzer(env.parent)
+
+        env.parent.set_variable(name.value, s.eval(value))
+        return Unit(AtomicType.NIL)
+
+
 env.set_function('print', Unit(AtomicType.FUNCTION, PrintFn()))
+env.set_function('defvar', Unit(AtomicType.MACRO, DefvarMc()))
